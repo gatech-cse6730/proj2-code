8975a4e59ed26e47e2a12a30e369c673527ae826
Matt May <matthewbentonmay@gmail.com>
2016-04-30 18:59:48 -0500

    analyzer update

--- code/analyzer.py
+++ code/analyzer.py
@@ -29,19 +29,23 @@ class Analyzer(object):
         ax.set_xlabel('Parameter Set')
         ax.set_ylabel('Mean Growth Rate')
 
-
         a=np.arange(1,4,1)
         b=np.arange(14,17,1)
+        d=np.arange(42,45,1)
         c=[1,2,3]
 
         ax.errorbar(a,b,yerr=c)
         ax.scatter(a,b,s=40)
+        ax.plot(a,b)
+        
+        ax.errorbar(a,d,yerr=c)
+        ax.scatter(a,d,s=40)
+        ax.plot(a,d)
 
         plt.xticks(np.arange(np.min(a), np.max(a)+1, 1))
-        rname = 'results-%d.png' % i
-        print rname
-        plt.savefig(rname, bbox_inches='tight')
+        plt.savefig('results.png', bbox_inches='tight')
 
 if __name__ == '__main__':
     analyzer = Analyzer()
     analyzer.prepare_results()
+    analyzer.prepare_plots()
