552a2d0900b467017232b99262eeb172572f0e55
Matt May <matthewbentonmay@gmail.com>
2016-05-02 12:52:40 -0500

    cleanup

--- code/batch_driver.py
+++ code/batch_driver.py
@@ -2,6 +2,11 @@ from driver import Driver
 import numpy as np
 
 class BatchDriver(object):
+    """
+    Performs a batch of simulation runs. Only for use by analyzer.py.
+    
+    """
+
     def __init__(self, num_sims=20):
         self.num_sims = num_sims
 
@@ -14,15 +19,12 @@ class BatchDriver(object):
         print('---> Initializing batch for initial_pop %d.', initial_pop)
 
         for i in xrange(self.num_sims):
-
             print('---> Initializing simulation run %d for initial pop %d.' % (i, initial_pop))
 
             driver = Driver(vis=False)
-
             result = driver.drive(max_iterations=1500,
                                   random_seed=random_seed,
                                   initial_pop=initial_pop)
-
             results.append(result)
 
             random_seed += 2
